7dff6b2dc324663a5b1bc6447c21455a03b17708
AJ <edulife1999@gmail.com>
2026-01-26 18:38:18 +0600

    changes

--- resources/views/admin/layouts/invoice.blade.php
+++ resources/views/admin/layouts/invoice.blade.php
@@ -210,13 +210,13 @@
                         <!-- Invoice To -->
                     <div class="invoice-to" style=" ">
                         <h4 style="font-size:12px;">Invoice To:</h4>
-                        <p style="font-size:11px; line-height: 1.2px; letter-spacing: 0.2px;">
+                        <p style="font-size:11px; line-height: 1.4px; letter-spacing: 0.2px;">
                             {{-- <strong>Name:</strong> --}}
                             {{ $order->name }}</p>
-                        <p style="font-size:11px; line-height: 1.2px; letter-spacing: 0.2px;">
+                        <p style="font-size:11px; line-height: 1.4px; letter-spacing: 0.2px;">
                             {{-- <strong>Phone:</strong> --}}
                             {{ $order->phone }}</p>
-                        <p style="font-size:11px; line-height: 1.2px; letter-spacing: 0.2px;">
+                        <p style="font-size:11px; line-height: 1.4px; letter-spacing: 0.2px;">
                             <strong>Address:</strong>{{ $order->address }}, {{ $order->thana }}, {{ $order->district }}</p>
                     </div>
 

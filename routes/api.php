3219be6f75cf0cae87c44b9adb6ece16cb6203bc
AJ <edulife1999@gmail.com>
2026-04-02 11:03:15 +0600

    changes

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,7 @@ use App\Http\Controllers\API\HomepageSectionController;
 
 // Public routes
 
-Route::middleware(['throttle:60,1'])->group(function () {
+// Route::middleware(['throttle:200,1'])->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 // Categories
@@ -104,4 +104,4 @@ Route::get('/pages', [PageController::class, 'index']);
 Route::get('/pages/{slag}', [PageController::class, 'show']);
 
 
-});
+// });
